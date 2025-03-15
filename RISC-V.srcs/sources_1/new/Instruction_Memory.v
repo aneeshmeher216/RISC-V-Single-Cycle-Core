d58cb44a4fa7c426eced6832ac5be3b25dd05116
Aneesh Meher <138611502+aneeshmeher216@users.noreply.github.com>
2025-03-15 20:06:28 +0530

    Added a comment in Instruction_Memory.v

--- RISC-V.srcs/sources_1/new/Instruction_Memory.v
+++ RISC-V.srcs/sources_1/new/Instruction_Memory.v
@@ -9,13 +9,13 @@ module Instr_Mem(
  //Meomory Creation
  reg [31:0] mem[1023:0];
  
- assign RD = (rst == 1'b0) ? 32'h00000000 : mem[A[31:2]];
+ assign RD = (rst == 1'b0) ? 32'h00000000 : mem[A[31:2]];    // becuase we have designed memory as word addressable
  
  initial begin
 //    mem[0] = 32'hFFC4A303;
 //    mem[1] = 32'h00832383;
     mem[0] = 32'h0064A423;
-    mem[0] =32'h0062E233;
+  mem[1] =32'h0062E233;
  end
  
 endmodule
